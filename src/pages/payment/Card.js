ade567faca96f8d7fd92de6ccddee406215adf95
yaSMon9N9 <yasminsoso123123@gmail.com>
2024-06-09 16:32:32 +0300

    -_-

--- src/pages/payment/Card.js
+++ src/pages/payment/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import Cards from "./cardd";
 
 const Container = styled.div`
   .rccs {
---
+++ src/pages/payment/cardd.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function cardd() {
+  return (
+    <div>cardd</div>
+  )
+}
+
+export default cardd
